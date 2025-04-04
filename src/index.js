086196c6943f90c293ae6ab501275f1bae831403
quyenkol_designer <111137192+ducquyen12312ew@users.noreply.github.com>
2025-04-05 00:10:28 +0700

    a

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("home");
+    res.render("startuphome");
 });
 
 app.get("/login", (req, res) => {
